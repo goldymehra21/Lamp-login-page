77db59be8628098fd9fa5e385e2ce959dc26120a
Goldy Mehra <mehragoldy233@gmail.com>
2026-07-31 23:58:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const lamp = document.getElementById("lamp");
+// Lamp Login Animation
+// Goldy Mehra
 
-    lamp.addEventListener("click", function () {
-        document.body.classList.toggle("light-off");
-    });
+const body = document.body;
+const chain = document.getElementById("chain");
+
+let lightOn = false;
+
+chain.addEventListener("click", () => {
+
+    // Toggle light
+    lightOn = !lightOn;
+
+    if(lightOn){
+
+        body.classList.add("light-on");
+
+    }else{
+
+        body.classList.remove("light-on");
+
+    }
+
+});
+
+// Pull chain animation
+
+chain.addEventListener("mousedown",()=>{
+
+    chain.style.transform="translateY(15px)";
+
+});
+
+chain.addEventListener("mouseup",()=>{
+
+    chain.style.transform="translateY(0px)";
+
+});
+
+chain.addEventListener("mouseleave",()=>{
+
+    chain.style.transform="translateY(0px)";
+
+});
+
+// Mobile Touch Support
+
+chain.addEventListener("touchstart",()=>{
+
+    chain.style.transform="translateY(15px)";
+
+});
+
+chain.addEventListener("touchend",()=>{
+
+    chain.style.transform="translateY(0px)";
+
 });
